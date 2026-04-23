894f74a80771fd1382affab22c72ebf851b3ec96
XXRV0423 <xxavierrv@gmail.com>
2026-04-23 10:59:02 -0400

    feat: add main loop to drive the shell

--- shell.c
+++ shell.c
@@ -98,3 +98,34 @@ void execute_command(char *cmd)
                 waitpid(pid, status, 0);
         }
 }
+
+/**
+ * main - Entry point for simple_shell
+ *
+ * Repeatedly display a prompt, reads a command,and
+ * executes it until EOF is recieved or a read error occurs.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+        char *line;
+
+        while (1)
+        {
+                display_prompt();
+
+                line = read_line();
+                if (line == NULL)
+                        break;
+
+                strip_newline(line);
+
+                if (line[0] != '\0')
+                        execute_command(line);
+
+                free(line);
+        }
+
+        return (0);
+}
